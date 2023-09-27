ab878054c61054fa1c836ba05a3ee4f5ed986fff
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-27 20:24:18 +0000

    Make the views more dynamic

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,8 +3,18 @@ class ActorsController < ApplicationController
   def get_actors
 
     @actors = Actor.all
+    @type = "actors"
 
-    render({:template => "actors/all_actors"})
+    render({:template => "individuals/all_individuals"})
+  end
+
+  def get_actor
+    @id = params.fetch("id")
+    @type = "Actor"
+
+    @actor_info = Actor.where({:id => @id}).at(0)
+
+    render({:template => "individuals/single_individual"})
   end
 
 end
