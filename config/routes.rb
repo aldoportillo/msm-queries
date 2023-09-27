091ca41535c3cdb06e9c8e816fd6b7ce5024c025
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-27 20:09:37 +0000

    Add dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", :controller => "directors", :action => "get_directors")
+  get("/directors/:id", :controller => "directors", :action => "get_director")
   get("/directors/oldest", :controller => "directors", :action => "get_oldest_director")
   get("/directors/youngest", :controller => "directors", :action => "get_youngest_director")
 
   get("/movies", :controller => "movies", :action => "get_movies")
+  get("/movies/:id", :controller => "movies", :action => "get_movie")
 
   get("/actors", :controller => "actors", :action => "get_actors")
+  get("/actors/:id", :controller => "actors", :action => "get_actor")
 
 end
