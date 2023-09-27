9eb48a9720645a3b1345f8980449ef489889a03d
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-27 21:21:23 +0000

    Create get movies route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
 
   def get_movies
-  
+    @movies = Movie.all
+
+    render({:template => "movies/all_movies"})
+  end
+
+  def get_movie
+
   end
 
 end
