9a8b232a8b362fc1216520f7d95dee8744c43dd0
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-27 20:44:14 +0000

    Fix route order

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", :controller => "directors", :action => "get_directors")
-  get("/directors/:id", :controller => "directors", :action => "get_director")
   get("/directors/oldest", :controller => "directors", :action => "get_oldest_director")
   get("/directors/youngest", :controller => "directors", :action => "get_youngest_director")
+  get("/directors/:id", :controller => "directors", :action => "get_director")
 
   get("/movies", :controller => "movies", :action => "get_movies")
   get("/movies/:id", :controller => "movies", :action => "get_movie")
