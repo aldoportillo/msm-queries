57c89bd23b5c236cc970fc35eec474dcfb989c06
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-27 16:57:14 +0000

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", :controller => "directors", :action => "get_directors")
+  get("/directors/oldest", :controller => "directors", :action => "get_oldest_director")
+  get("/directors/youngest", :controller => "directors", :action => "get_youngest_director")
+
+  get("/movies", :controller => "movies", :action => "get_movies")
+
+  get("/actors", :controller => "actors", :action => "get_actors")
+
 end
