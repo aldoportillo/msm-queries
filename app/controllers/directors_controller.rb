f0aaf672aa39d68996e4bb4b14bbf71cc58c9029
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-27 20:26:38 +0000

    Write actions to get all directors and individual directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,19 @@
 class DirectorsController < ApplicationController
 
   def get_directors
+    @directors = Director.all
+    @type = "directors"
+
+    render({:template => "individuals/all_individuals"})
+  end
+
+  def get_director
+    @id = params.fetch("id")
+    @type = "Director"
+
+    @director_info = Director.where({:id => @id}).at(0)
+
+    render({:template => "individuals/single_individual"})
   end
 
   def get_oldest_director
